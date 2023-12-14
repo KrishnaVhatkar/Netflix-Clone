4c3eed286ffae8c97741388ef82c043dbba78f68
KrishnaVhatkar <119169766+KrishnaVhatkar@users.noreply.github.com>
2023-12-14 11:14:21 +0530

    first commit

---
+++ netflix-api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://Krishna:Krish_1323@cluster0.q06cohl.mongodb.net/My-Netflix?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+    app.listen(5000, () => {
+      console.log("server started on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
